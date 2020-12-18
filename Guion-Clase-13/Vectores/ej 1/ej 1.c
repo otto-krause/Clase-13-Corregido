0290ecd2528507f66c2c7dee7b9f4c2a5c24cc9c
Marco Flores <marcofloresantonio899@gmail.com>
2020-12-17 23:17:17 -0300

    Update ej 1.c

--- Guion-Clase-13/Vectores/ej 1/ej 1.c
+++ Guion-Clase-13/Vectores/ej 1/ej 1.c
@@ -3,32 +3,21 @@
 
 int main()
 {
-
 	int v[5],acu=0,z;
-
 	ing(v);
-
 	for(z=0;z<5;z++)
 	{
-
 	printf("\n La resistencia %d es : %d ",z,v[z]);
 	acu=acu+v[z];
 	}
-
 	printf("\n La resistencia total es %d ",acu);
-
 	}
-
-
 	int ing(int *a)
 	{
-
 	int i;
-
-	for (i=0;i<5;i++){
-
+	for (i=0;i<5;i++)
+	{
 		printf("\n Ingrese una resistencia ");
 		scanf("%d",&a[i]);
 	}
-
-	}
+}
